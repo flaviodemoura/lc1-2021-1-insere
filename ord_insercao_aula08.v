5b036e801447f96ff3674b54c86a67c2d550c91a
sindeto <52028532+sindeto@users.noreply.github.com>
2021-08-22 23:28:20 -0300

    Update ord_insercao_aula08.v

--- ord_insercao_aula08.v
+++ ord_insercao_aula08.v
@@ -1,4 +1,4 @@
-(** Algoritmo de Ordenação por Inserção em listas *)
+
 
 Require Import List Arith.
 Open Scope nat_scope.
@@ -10,6 +10,11 @@ Fixpoint insere (n:nat) (l: list nat) :=
              else (h :: (insere n tl)) (* inserção de n na cauda da lista *)
   end.
 
+Print nat.
+Check nat_ind.
+Print list.
+Check list_ind.
+
 Compute (insere 11 (10 :: 10 :: 10 :: nil)).
 Compute (insere 0 (3 :: 9 :: 10 :: nil)).
 Compute (insere 11 (10 :: 9 :: nil)).
@@ -19,11 +24,95 @@ Inductive ordenada: list nat -> Prop :=
 | ord_one: forall x, ordenada (x :: nil)
 | ord_all: forall x y l, x <= y -> ordenada (y :: l)  -> ordenada (x :: y :: l).
 
+Check ordenada_ind.
+
+
 Lemma insere_preserva_ordem: forall l x, ordenada l -> ordenada (insere x l). 
 Proof.
   induction l.
   - intros x H.
     simpl.
     apply ord_one.
-  - Admitted.
+  - intros x H.
+    simpl.
+    destruct (x <=? a) eqn:Hle.
+    + apply ord_all.
+      * apply leb_complete.
+        assumption.
+      * assumption.
+    + generalize dependent l.
+      intro l.
+      case l.
+      * intros IH H.
+        simpl.
+        apply ord_all.
+        ** apply leb_complete_conv in Hle.
+           apply Nat.lt_le_incl.
+           assumption.
+        ** apply ord_one.
+      * intros n l' IH H.
+        simpl.
+        destruct (x <=? n) eqn:Hle'.
+        ** apply ord_all.
+           *** apply leb_complete_conv in Hle.
+               apply Nat.lt_le_incl.
+               assumption.
+           *** apply ord_all.
+               **** apply leb_complete.
+                    assumption.
+               **** inversion H; subst.
+                    assumption.
+        ** inversion H; subst.
+           apply ord_all.
+           *** assumption.
+           *** specialize (IH x).
+               simpl in IH.
+               rewrite Hle' in IH.
+               apply IH.
+               assumption.
+Qed.
 
+Lemma insere_preserva_ordem': forall l x, ordenada l -> ordenada (insere x l). 
+Proof.
+induction l.
+  -intros x H.
+  simpl.
+  apply ord_one.
+  -intros x H.
+   simpl.
+  destruct (x<=? a) eqn:Hleq.
+  +apply ord_all.
+  * apply leb_complete.
+        assumption.
+      * assumption.
+    + generalize dependent l.
+      intro l.
+      case l.
+      * intros IH H.
+        simpl.
+        apply ord_all.
+        ** apply leb_complete_conv in Hleq.
+           apply Nat.lt_le_incl.
+           assumption.
+        ** apply ord_one.
+      * intros n l' IH H.
+        simpl.
+        destruct (x <=? n) eqn:Hleq'.
+        ** apply ord_all.
+           *** apply leb_complete_conv in Hleq.
+               apply Nat.lt_le_incl.
+               assumption.
+              *** apply ord_all.
+               **** apply leb_complete.
+                    assumption.
+               **** inversion H; subst.
+                    assumption.
+        ** inversion H; subst.
+           apply ord_all.
+           *** assumption.
+           *** specialize (IH x).
+               simpl in IH.
+               rewrite Hleq' in IH.
+               apply IH.
+               assumption.
+Qed.
